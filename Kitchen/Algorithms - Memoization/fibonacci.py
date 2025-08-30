1b9c4d8f2c1d37240202d5a45a25a039909ece6e
Golden_Leaves <letuongminh10@gmail.com>
2025-08-30 09:29:34 +0700

    Implement naive fibonacci

--- Kitchen/Algorithms - Memoization/fibonacci.py
+++ Kitchen/Algorithms - Memoization/fibonacci.py
@@ -1,14 +1,20 @@
 from runtime_utils import track_time
+import sys
+sys.set_int_max_str_digits(100000)
 @track_time
 def naive_fib(n):
     if n == 0:
-        return [0]
+        return 0
     elif n == 1:
-        return [0,1]
-    
-    fib_nums = [0,1]
-    for i in range(2,n+1):
-        fib_nums.append(fib_nums[i-1] + fib_nums[i-2])
-    return fib_nums[n]
+        return 1
+    a,b = 0,1
 
-print(naive_fib(200000))
+    for _ in range(1,n):
+        temp_b = b
+        b = a + b
+        a = temp_b
+
+    return b
+        
+
+print(naive_fib(10))
